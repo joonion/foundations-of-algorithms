bd06035990dbf69a7c598dc183b529f80076c367
joonion <joonion@gmail.com>
2021-05-18 19:30:53 +0900

    Chapter 4

--- Chap.04/4.3_Dijkstra.cpp
+++ Chap.04/4.3_Dijkstra.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 using namespace std;
 
 #define INF 0xffff
@@ -8,29 +7,29 @@ using namespace std;
 typedef vector<vector<int>> matrix_t;
 typedef vector<pair<int, int>> set_of_edges;
 
-void prim(int n, matrix_t& W, set_of_edges& F)
+void dijkstra(int n, matrix_t& W, set_of_edges& F)
 {
     int vnear, min;
-    vector<int> nearest(n + 1), dist(n + 1);
+    vector<int> touch(n + 1), length(n + 1);
     F.clear();
     for (int i = 2; i <= n; i++) {
-        nearest[i] = 1;
-        dist[i] = W[1][i];
+        touch[i] = 1;
+        length[i] = W[1][i];
     }
     for (int t = 1; t <= n - 1; t++) {
         min = INF;
         for (int i = 2; i <= n; i++)
-            if (0 <= dist[i] && dist[i] < min) {
-                min = dist[i];
+            if (0 <= length[i] && length[i] < min) {
+                min = length[i];
                 vnear = i;
             }
-        F.push_back(make_pair(vnear, nearest[vnear]));
-        dist[vnear] = -1;
+        F.push_back(make_pair(touch[vnear], vnear));
         for (int i = 2; i <= n; i++)
-            if (dist[i] > W[i][vnear]) {
-                dist[i] = W[i][vnear];
-                nearest[i] = vnear;
+            if (length[i] > length[vnear] + W[vnear][i]) {
+                length[i] = length[vnear] + W[vnear][i];
+                touch[i] = vnear;
             }
+        length[vnear] = -1;
     }
 }
 
@@ -42,11 +41,11 @@ int main() {
         W[i][i] = 0;
     while (m-- > 0) {
         cin >> u >> v >> w;
-        W[u][v] = W[v][u] = w;
+        W[u][v] = w; // a directed graph.
     }
 
     set_of_edges F;
-    prim(n, W, F);
+    dijkstra(n, W, F);
     for (auto e: F)
         cout << e.first << " " << e.second << " " << W[e.first][e.second] << endl;
 }

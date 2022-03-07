7304c65f9ed90e8e7359c3c9bf6c9ea0ad8b8303
Joonion Bae <joonion@gmail.com>
2022-03-08 05:19:15 +0900

    Modified Strassen

--- Chap.01/1.2_AddingArrayMembers.cpp
+++ Chap.01/1.2_AddingArrayMembers.cpp
@@ -2,16 +2,14 @@
 #include <vector>
 using namespace std;
 
-int sum(int n, vector<int>& S) 
-{
+int sum(int n, vector<int>& S) {
     int result = 0;
     for (int i = 1; i <= n; i++)
         result += S[i];
     return result;
 }
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
     vector<int> S(n + 1);

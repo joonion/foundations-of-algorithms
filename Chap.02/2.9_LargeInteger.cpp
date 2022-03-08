ef23bbf4f018ebb81a590598c7f5c10ddb381e5c
Joonion Bae <joonion@gmail.com>
2022-03-08 09:15:29 +0900

    Large Integer Bug Fix

--- Chap.02/2.9_LargeInteger.cpp
+++ Chap.02/2.9_LargeInteger.cpp
@@ -1,50 +1,42 @@
-/*
- * Consider only positive inputs as a string.
- * Note that 0 should be given as a string "0".
- */
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-typedef vector<int> large_integer;
-
+typedef vector<int> LargeInteger;
 const int threshold = 1;
 
-void print_large_integer(large_integer& v) {
-    for (int i = v.size() - 1; i >= 0; i--)
-        cout << v[i] << " ";
+void print_large_integer(LargeInteger v) {
+    if (v.size() == 0)
+        cout << 0;
+    else
+        for (int i = 1; i <= v.size(); i++)
+            cout << v[v.size() - i];
     cout << endl;
 }
 
-void assign_value(large_integer& u, string s) 
-{
-    u.resize(0);
-    for (int i = s.size() - 1; i >= 0; i--)
-        u.push_back(s[i] - '0');
+void make_large_integer(string s, LargeInteger& v) {
+    v.resize(s.size());
+    for (int i = 1; i <= v.size(); i++)
+        v[v.size() - i] = s[i - 1] - '0';
 }
 
-bool is_zero(large_integer& u)
-{
-    if (u.size() == 1 && u[0] == 0)
-        return true;
-    return false;
+void remove_leading_zeros(LargeInteger& v) {
+    while (v.back() == 0)
+        v.pop_back();
 }
 
-void roundup_carry(large_integer& a)
-{
+void roundup_carry(LargeInteger& v) {
     int carry = 0;
-    for (int i = 0; i < a.size(); i++) {
-        a[i] += carry;
-        carry = a[i] / 10;
-        a[i] = a[i] % 10;
+    for (int i = 0; i < v.size(); i++) {
+        v[i] += carry;
+        carry = v[i] / 10;
+        v[i] = v[i] % 10;
     }
     if (carry != 0)
-        a.push_back(carry);
+        v.push_back(carry);
 }
 
-void ladd(large_integer& a, large_integer& b, large_integer& c)
-{
+void ladd(LargeInteger a, LargeInteger b, LargeInteger& c) {
     c.resize(max(a.size(), b.size()));
     fill(c.begin(), c.end(), 0);
     for (int i = 0; i < c.size(); i++) {
@@ -54,8 +46,7 @@ void ladd(large_integer& a, large_integer& b, large_integer& c)
     roundup_carry(c);
 }
 
-void lmult(large_integer& a, large_integer& b, large_integer& c)
-{
+void lmult(LargeInteger a, LargeInteger b, LargeInteger& c) {
     c.resize(a.size() + b.size() - 1);
     fill(c.begin(), c.end(), 0);
     for (int i = 0; i < a.size(); i++)
@@ -64,11 +55,9 @@ void lmult(large_integer& a, large_integer& b, large_integer& c)
     roundup_carry(c);
 }
 
-void pow_by_exp(large_integer& u, int m, large_integer &v)
-{
-    if (is_zero(u)) {
-        v.resize(1); v[0] = 0;
-    }
+void pow_by_exp(LargeInteger u, int m, LargeInteger &v) {
+    if (u.size() == 0)
+        v.resize(0);
     else {
         v.resize(u.size() + m);
         fill(v.begin(), v.end(), 0);
@@ -76,47 +65,38 @@ void pow_by_exp(large_integer& u, int m, large_integer &v)
     }
 }
 
-void div_by_exp(large_integer& u, int m, large_integer &v)
-{
-    if (is_zero(u) || u.size() <= m) {
-        v.resize(1); v[0] = 0;
-    }
+void div_by_exp(LargeInteger& u, int m, LargeInteger &v) {
+    if (u.size() <= m)
+        v.resize(0);
     else {
         v.resize(u.size() - m);
         copy(u.begin() + m, u.end(), v.begin());
     }
 }
 
-void rem_by_exp(large_integer& u, int m, large_integer &v)
-{
-    if (is_zero(u)) {
-        v.resize(1); v[0] = 0;
-    }
+void rem_by_exp(LargeInteger u, int m, LargeInteger &v) {
+    if (u.size() == 0)
+        v.resize(0);
     else {
-        v.resize(m);
-        copy(u.begin(), u.begin() + m, v.begin());
-        // remove leading zeros
-        while (v.size() > 1 && v.back() == 0)
-            v.pop_back();
+        // Note that u.size() can be smaller than m.
+        int k = m < u.size() ? m : u.size();
+        v.resize(k);
+        copy(u.begin(), u.begin() + k, v.begin());
+        remove_leading_zeros(v);
     }
 }
 
-void prod(large_integer& u, large_integer& v, large_integer &r)
-{
-    large_integer x, y, w, z;
-    large_integer t1, t2, t3, t4, t5, t6, t7, t8;
-
+void prod(LargeInteger u, LargeInteger v, LargeInteger &r) {
+    LargeInteger x, y, w, z;
+    LargeInteger t1, t2, t3, t4, t5, t6, t7, t8;
     int n = max(u.size(), v.size());
-    if (is_zero(u) || is_zero(v)) {
-        r.resize(1); r[0] = 0;
-    }
-    else if (min(u.size(), v.size()) <= threshold)
+    if (u.size() == 0 || v.size() == 0)
+        r.resize(0);
+    else if (n <= threshold)
         lmult(u, v, r);
     else {
         int m = n / 2;
         div_by_exp(u, m, x); rem_by_exp(u, m, y);
-        print_large_integer(x);
-        print_large_integer(y);        
         div_by_exp(v, m, w); rem_by_exp(v, m, z);
         // t2 <- prod(x,w) * 10^(2*m)
         prod(x, w, t1); pow_by_exp(t1, 2 * m, t2); 
@@ -127,16 +107,12 @@ void prod(large_integer& u, large_integer& v, large_integer &r)
     }
 }
 
-int main() 
-{
-    string s1, s2;
-    cin >> s1 >> s2;
-    large_integer u, v;
-    assign_value(u, s1);
-    assign_value(v, s2);
-    print_large_integer(u);
-    print_large_integer(v);
-    large_integer r;
+int main() {
+    string su, sv;
+    cin >> su >> sv;
+    LargeInteger u, v, r;
+    make_large_integer(su, u);
+    make_large_integer(sv, v);
     prod(u, v, r);
     print_large_integer(r);
 }

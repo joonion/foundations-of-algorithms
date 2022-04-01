505f77e9ab6b956a87b7c9760176042a5fb766df
Joonion Bae <joonion@gmail.com>
2022-04-01 13:17:07 +0900

    Chapter 3

--- Chap.03/3.0_Fibonacci.2.cpp
+++ Chap.03/3.0_Fibonacci.2.cpp
@@ -6,21 +6,18 @@ typedef unsigned long long longint;
 vector<longint> F;
 
 longint fib2(int n) {
-    if (n >= F.size()) {
-        int start = F.size();
-        F.resize(n + 1);
-        if (n < 2)
-            F[n] = n;
-        else {
-            for (int i = start; i <= n; i++)
-                F[i] = F[i - 1] + F[i - 2];
-        }
-    }
+    if (n <= 1)
+        F[n] = n;
+    else if (F[n] == -1)
+        F[n] = fib2(n - 1) + fib2(n - 2);
     return F[n];
 }
 
 int main() {
     int n; cin >> n;
-    for (int i = 0; i <= n; i++)
-        cout << fib2(i) << endl;
+    cout << n << endl;
+    for (int i = 0; i <= n; i++) {
+        F.resize(i + 1, -1);
+        cout << i << ": " << fib2(i) << endl;
+    }
 }
--- Chap.03/3.0_Fibonacci.3.cpp
+++ Chap.03/3.0_Fibonacci.3.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <map>
+#include <vector>
 using namespace std;
 
 typedef unsigned long long longint;
-map<int, longint> F;
+vector<longint> F;
 
 longint fib3(int n) {
-    if (F.find(n) != F.end())
-        return F[n];
+    F.resize(n + 1);
+    if (n <= 1)
+        F[n] = n;
     else {
-        if (n < 2)
-            F[n] = n;
-        else
-            F[n] = fib3(n - 1) + fib3(n - 2);
-        return F[n];
+        F[0] = 0; F[1] = 1;
+        for (int i = 2; i <= n; i++)
+            F[n] = F[n - 1] + F[n - 2];
     }
+    return F[n];
 }
 
 int main() {
     int n; cin >> n;
     for (int i = 0; i <= n; i++)
-        cout << fib3(i) << endl;
+        cout << i << ": " << fib3(i) << endl;
 }
